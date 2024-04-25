fdb212b1348cec642ee8fba4b0bcb5968c8f0ff0
AngusWG <z740713651@outlook.com>
2024-04-25 16:54:15 +0800

    feature: update versioneer

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
@@ -2,9 +2,9 @@
 # encoding: utf-8
 from __future__ import print_function
 
-from ._version import get_versions
+from . import _version
+
+__version__ = _version.get_versions()['version']
 
 __author__ = "{{ cookiecutter.github_username }}"
 __email__ = "{{ cookiecutter.email }}"
-__version__ = get_versions()["version"]
-del get_versions
