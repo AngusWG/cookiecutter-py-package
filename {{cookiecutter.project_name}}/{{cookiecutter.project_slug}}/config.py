ee1c6037b3b0501736b5ccc47e83989770b5478a
AngusWG <z740713651@outlook.com>
2024-04-25 17:28:42 +0800

    feature: config read from env

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/config.py
@@ -3,6 +3,9 @@
 import os
 
 import yaml
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config:
@@ -14,7 +17,7 @@ class Config:
         "[%(asctime)s] [%(uuid)s] [%(threadName)s:%(thread)d] [%(levelname)s]: "
         "%(message)s [%(pathname)s <%(lineno)d>]"
     )
-    LOG_LEVEL = "DEBUG"
+    LOG_LEVEL = "INFO"
 
     # import os ; print(os.urandom(24))
     SECRET_KEY = b""
@@ -32,23 +35,32 @@ class Config:
     sentry_dns = None
     redis_url = "redis://127.0.0.1:6379"
 
+    {{cookiecutter.project_slug}}_config_path = "{{cookiecutter.project_slug}}_config.yaml"
+
     def __init__(self):
         """
         >>> os.environ["{{cookiecutter.project_slug}}_config_path"] = os.path.abspath("{{cookiecutter.project_slug}}_config.yaml")
         >>> Config()
         read config.yaml from...
         """
-        _default_path = "{{cookiecutter.project_slug}}_config.yaml"
-        config_path = os.environ.get("{{cookiecutter.project_slug}}_config_path", _default_path)
-        if config_path != _default_path:
-            print("read config.yaml from : {}".format(config_path))
+        print("=== Prepare {{cookiecutter.project_slug}} config start ===")
 
-        if os.path.exists(config_path):
-            with open(config_path, "r", encoding="utf8") as f:
+        # read config from env
+        uppercase_vars = [var for var in vars(Config) if not var.startswith("__")]
+
+        for var_name in uppercase_vars:
+            env_value = os.environ.get(var_name)
+            if env_value is not None:
+                print(f"read from ENVIRON: var_name = {env_value}")
+                setattr(self, var_name, env_value)
+
+        # read config from config file
+        if os.path.exists(self.{{cookiecutter.project_slug}}_config_path):
+            with open(self.{{cookiecutter.project_slug}}_config_path, "r", encoding="utf8") as f:
                 entries = yaml.safe_load(f)
             self.__dict__.update(entries or {})
-            print("read {} values:".format(config_path))
-            print(self.__dict__)
+            print("read {} values: {}".format(self.{{cookiecutter.project_slug}}_config_path), entries)
 
+        print("=== Prepare {{cookiecutter.project_slug}} config finish===")
 
 conf = Config()
